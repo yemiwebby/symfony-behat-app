ac6f0ed407ca5b066ccab46df03410a5371c392a
yemiwebby <yemiwebby@gmail.com>
2020-06-19 04:46:11 +0100

    initial commit

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CustomerController extends AbstractController
+{
+    /**
+     * @Route("/customer", name="customer")
+     */
+    public function index()
+    {
+        $customers = [
+            [
+                'id' => 1,
+                'name' => 'Olususi Oluyemi',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Camila Terry',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Joel Williamson',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            ],
+            [
+                'id' => 4,
+                'name' => 'Deann Payne',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            ],
+            [
+                'id' => 5,
+                'name' => 'Donald Perkins',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+            ]
+        ];
+
+        $response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+        $response->setContent(json_encode($customers));
+
+        return $response;
+    }
+}
